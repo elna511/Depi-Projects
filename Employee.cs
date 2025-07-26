355176e2ced614cde1dc941856cfc633ee26b7bc
cds.NadaHassan24651@alexu.edu.eg <cds.NadaHassan24651@alexu.edu.eg>
2025-07-26 23:05:20 +0300

    update 1

---
+++ Employee.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Depi_c__Task4
+{
+    public class Employee
+    {
+        private string _Name;
+        private int _Id;
+        private string _JobTitle;
+        private double _Salary;
+        private DateTime _DateOfBirth;
+        private DateTime _HireDate;
+        private string _Address;
+        private string _Department;
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+        public int Id
+        {
+            get { return _Id; }
+            set { _Id = value; }
+        }
+        public string JobTitle
+        {
+            get { return _JobTitle; }
+            set { _JobTitle = value; }
+        }
+        public double Salary
+        {
+            get { return _Salary; }
+            set { _Salary = value; }
+        }
+        public DateTime HireDate
+        {
+            get { return _HireDate; }
+            set { _HireDate = value; }
+        }
+        public DateTime DateOfBirth
+        {
+            get { return _DateOfBirth; }
+            set { _DateOfBirth = value; }
+        }
+        public string Address
+        {
+            get { return _Address; }
+            set { _Address = value; }
+        }
+        public string Department
+        {
+            get { return _Department; }
+            set { _Department = value; }
+        }
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Job Title: {JobTitle}");
+            Console.WriteLine($"Salary: {Salary}");
+            Console.WriteLine($"Date of Birth: {DateOfBirth.ToShortDateString()}");
+            Console.WriteLine($"Hire Date: {HireDate.ToShortDateString()}");
+            Console.WriteLine($"Address: {Address}");
+            Console.WriteLine($"Department: {Department}");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Employee Nada = new Employee();
+            Employee Ahmed = new Employee();
+            Nada.Name = "Nada Hassan";
+            Nada.Id = 1;
+            Nada.JobTitle = "Data Scientist";
+            Nada.Salary = 30000;
+            Nada.DateOfBirth = new DateTime(2004, 11, 5);
+            Nada.HireDate = new DateTime(2023, 1, 1);
+            Nada.Address = "123 Main St, Alex, Egypt";
+            Nada.Department = "Data Science";
+            Console.WriteLine("Employees Information:");
+            Console.WriteLine();
+            Nada.DisplayEmployeeInfo();
+            Console.WriteLine();
+            Ahmed.Name = "Ahmed Ali";
+            Ahmed.Id = 2;
+            Ahmed.JobTitle = "Software Engineer";
+            Ahmed.Salary = 25000;
+            Ahmed.DateOfBirth = new DateTime(2003, 5, 15);
+            Ahmed.HireDate = new DateTime(2023, 2, 1);
+            Ahmed.Address = "456 Elm St, Cairo, Egypt";
+            Ahmed.Department = "IT";
+            Ahmed.DisplayEmployeeInfo();
+
+            
         }
     }
 }
