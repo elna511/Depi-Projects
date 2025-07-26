c1d391dc31e27958bd47f479e55d9039c25f0961
Nada Hassan <Nada Hassan@NADAHASSAN>
2025-07-26 21:23:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Depi_c__Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
